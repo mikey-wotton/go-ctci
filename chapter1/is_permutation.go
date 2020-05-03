a4f5fe1120bd69f28a51e3930a80fe6809470847
mikehpoo <mikenw@hotmail.co.uk>
2020-05-03 19:19:00 +0100

    implemented and tested isPermutation

--- chapter1/is_permutation.go
+++ chapter1/is_permutation.go
@@ -1,6 +1,10 @@
 package arraysandstrings
 
+import "sort"
+
 /*
+  Check Permutation: Given two strings, write a method to decide if one is a
+  permutation of the other.
 
   Hint #1: Describe what it means for two strings to be permutations of each other.
   Now, look at that definition you provided. Can you check the strings against
@@ -13,6 +17,60 @@ package arraysandstrings
 
   Hint #131: Can you do all three checks in a single pass?
 */
-func is_permutation(s string, p string) bool {
-	return false
+// Quicksort: O(n log n)
+func isPermutation(s string, p string) bool {
+	if len(s) != len(p) {
+		return false
+	}
+
+	s, p = sortString(s), sortString(p)
+	for i := range s {
+		if s[i] != p[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// One time pass but extra space required: O(N)
+func isPermutationExtraSpace(s string, p string) bool {
+	if len(s) != len(p) {
+		return false
+	}
+
+	seen := [256]byte{}
+
+	for i := range s {
+		seen[s[i]]++
+		seen[p[i]]--
+	}
+
+	for _, count := range seen {
+		if count != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s sortRunes) Len() int {
+	return len(s)
+}
+
+func sortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
 }
--- chapter1/is_permutation_test.go
+++ chapter1/is_permutation_test.go
@@ -13,6 +13,11 @@ func Test_isPermutation(t *testing.T) {
 			perm: "olleh",
 			want: true,
 		},
+		"same length similar but not a permutation": {
+			str:  "qwertyuiop",
+			perm: "qwertyuiol",
+			want: false,
+		},
 		"different length strings cannot be permutations": {
 			str:  "aa",
 			perm: "a",
@@ -23,17 +28,66 @@ func Test_isPermutation(t *testing.T) {
 			perm: "qdsew",
 			want: true,
 		},
+		"empty string is a permutation": {
+			str:  "",
+			perm: "",
+			want: true,
+		},
 		"all repeating letters is a permutation ": {
-			str:  "rrrrrrrrrrrrrrrrr",
-			perm: "rrrrrrrrrrrrrrrrr",
+			str:  "rrrrrrrrrrr",
+			perm: "rrrrrrrrrrr",
 			want: true,
 		},
 	}
 
 	for desc, test := range tests {
-		if got := is_permutation(test.str, test.perm); got != test.want {
+		if got := isPermutation(test.str, test.perm); got != test.want {
 			t.Errorf("%s permutation test failed, got %v but want %v", desc, got, test.want)
 		}
 	}
+}
 
+func Test_isPermutationExtraSpace(t *testing.T) {
+	tests := map[string]struct {
+		str  string
+		perm string
+		want bool
+	}{
+		"backwards word is permutation": {
+			str:  "hello",
+			perm: "olleh",
+			want: true,
+		},
+		"same length similar but not a permutation": {
+			str:  "qwertyuiop",
+			perm: "qwertyuiol",
+			want: false,
+		},
+		"different length strings cannot be permutations": {
+			str:  "aa",
+			perm: "a",
+			want: false,
+		},
+		"jumbled letters are permutations": {
+			str:  "qwesd",
+			perm: "qdsew",
+			want: true,
+		},
+		"empty string is a permutation": {
+			str:  "",
+			perm: "",
+			want: true,
+		},
+		"all repeating letters is a permutation ": {
+			str:  "rrrrrrrrrrr",
+			perm: "rrrrrrrrrrr",
+			want: true,
+		},
+	}
+
+	for desc, test := range tests {
+		if got := isPermutationExtraSpace(test.str, test.perm); got != test.want {
+			t.Errorf("%s permutation test failed, got %v but want %v", desc, got, test.want)
+		}
+	}
 }
