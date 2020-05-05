e5117dcad10474d15257762e678566d8e7db21f0
mikehpoo <mikenw@hotmail.co.uk>
2020-05-05 08:07:51 +0100

    add chapter 1.4 palindrome tests and description

---
+++ chapter1/palindrome.go
@@ -0,0 +1,28 @@
+package arraysandstrings
+
+/*
+  Palindrome Permutation: Given a string, write a function to check if it is a
+  permutation of a palindrome. A palindrome is a word or phrase that is the same
+  forwards and backwards. A permutation is a rearrangement of letters.
+
+  The palindrome does not need to be limited to just dictionary words.
+
+  EXAMPLE
+  Input: Tact Coa
+  Output: True (permutations: "taco cat". "atco cta". etc.)
+
+  Hint #106: You do not have to-and should not-generate all permutations. This
+  would be very inefficient.
+
+  Hint #121: What characteristics would a string that is a permutation of a
+  palindrome have?
+
+  Hint #134: Have you tried a hash table? You should be able to get this down
+  to 0 (N) time.
+
+  Hint #136: Can you reduce the space usage by using a bit vector? 1
+*/
+
+func isPalindrome(s string) bool {
+	return false
+}
---
+++ chapter1/palindrome_test.go
@@ -0,0 +1,33 @@
+package arraysandstrings
+
+import "testing"
+
+func Test_isPalindrome(t *testing.T) {
+	tests := map[string]struct {
+		str  string
+		want bool
+	}{
+		"example test case provided": {
+			str:  "tact coa",
+			want: true,
+		},
+		"single letter in middle palindrome": {
+			str:  "madam",
+			want: true,
+		},
+		"dual letter middle palindrome": {
+			str:  "Never odd or even",
+			want: true,
+		},
+		"not a palindrome": {
+			str:  "beep",
+			want: false,
+		},
+	}
+
+	for desc, test := range tests {
+		if got := isPalindrome(test.str); got != test.want {
+			t.Errorf("%s test failed, got %v but want %v", desc, got, test.want)
+		}
+	}
+}
