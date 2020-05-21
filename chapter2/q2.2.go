b9c5074421d1fb8e685e547a4a8669a507c4b35c
mikey <mikenw@hotmail.co.uk>
2020-05-21 15:58:38 +0100

    add q2.2 tests and description

---
+++ chapter2/q2.2.go
@@ -0,0 +1,27 @@
+package chapter2
+
+/*
+  Return Kth to Last: Implement an algorithm to find the kth to last element of
+  a singly linked list.
+
+  Hint #8: What if you knew the linked list size? What is the difference between
+  finding the Kth-to last element and finding the Xth element?
+
+  Hint #25: If you don't know the linked list size, can you compute it? How does
+  this impact the runtime?
+
+  Hint #47: Look at this graph. Is there any node you can identify that will
+  definitely be okay to build first?
+
+  Hint #67: You might find it useful to return multiple values. Some languages
+  don't directly support this, but there are workarounds in essentially any
+  language. What are some of those workarounds?
+
+  Hint #726: We're probably going to run this algorithm many times. If we did
+  more preprocessing, is there a way we could optimize this?
+*/
+
+func returnKth(head *node) *node {
+
+	return nil
+}
---
+++ chapter2/q2.2_test.go
@@ -0,0 +1,36 @@
+package chapter2
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_returnKth(t *testing.T) {
+	tests := map[string]struct {
+		head *node
+		num  int
+		want *node
+	}{
+		"return just last node": {
+			head: &node{1, &node{2, nil}},
+			num:  0,
+			want: &node{2, nil},
+		},
+		"return all nodes": {
+			head: &node{1, &node{2, nil}},
+			num:  1,
+			want: &node{1, &node{2, nil}},
+		},
+		"return last 4 nodes": {
+			head: &node{1, &node{2, &node{3, &node{4, &node{5, &node{6, nil}}}}}},
+			num:  3,
+			want: &node{3, &node{4, &node{5, &node{6, nil}}}},
+		},
+	}
+
+	for desc, test := range tests {
+		if got := returnKth(test.head); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("%s test failed, got %v want %v", desc, got, test.want)
+		}
+	}
+}
